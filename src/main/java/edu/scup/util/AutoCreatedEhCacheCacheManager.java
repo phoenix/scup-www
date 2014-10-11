848a37626efb370e552346da70a3766c40db83f2
phoenix <wujunchen@gmail.com>
2014-10-11 10:20:42 +0800

    增加AutoCreatedEhCacheCacheManager

---
+++ src/main/java/edu/scup/util/AutoCreatedEhCacheCacheManager.java
@@ -0,0 +1,28 @@
+package edu.scup.util;
+
+import net.sf.ehcache.Ehcache;
+import org.springframework.cache.Cache;
+import org.springframework.cache.ehcache.EhCacheCache;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+
+/**
+ * 找不到对应的cache名时，默认创建一个该名字的ehCache并返回
+ */
+public class AutoCreatedEhCacheCacheManager extends EhCacheCacheManager {
+
+    @Override
+    public Cache getCache(String name) {
+        Cache cache = super.getCache(name);
+        if (cache == null) {
+            // check the EhCache cache again
+            // (in case the cache was added at runtime)
+            getCacheManager().addCache(name);
+            Ehcache ehcache = getCacheManager().getCache(name);
+            if (ehcache != null) {
+                cache = new EhCacheCache(ehcache);
+                addCache(cache);
+            }
+        }
+        return cache;
+    }
+}
