18c00175e4920a78be84f67d5f56011f3bdb9251
phoenix <wujunchen@gmail.com>
2014-11-07 09:26:34 +0800

    BusinessException的异常信息按照通用方式存放

--- src/main/java/edu/scup/web/exceptions/BusinessException.java
+++ src/main/java/edu/scup/web/exceptions/BusinessException.java
@@ -5,13 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class BusinessException extends RuntimeException {
-    private String msg;
+    private static final long serialVersionUID = 2814483140143382224L;
 
     public BusinessException(String msg) {
-        this.msg = msg;
-    }
-
-    public String getMsg() {
-        return msg;
+        super(msg);
     }
 }
