20d6aa267205260df58f9c9e5ff6010a9f38bd93
phoenix <wujunchen@gmail.com>
2014-11-10 16:11:00 +0800

    cas登陆可以关闭ssl证书校验

--- src/main/java/edu/scup/web/shiro/cas/ShiroCasRealm.java
+++ src/main/java/edu/scup/web/shiro/cas/ShiroCasRealm.java
@@ -12,8 +12,12 @@ import org.jasig.cas.client.validation.TicketValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Map;
 
@@ -21,9 +25,42 @@ public class ShiroCasRealm extends CasRealm {
     private static final Logger logger = LoggerFactory.getLogger(ShiroCasRealm.class);
     final String roleAttributeNames = "rolename";
     private String encoding = "GBK";
+    private boolean trustAllSSL;
 
     public ShiroCasRealm() {
         setRoleAttributeNames(roleAttributeNames);
+    }
+
+    @Override
+    protected void onInit() {
+        super.onInit();
+        if (trustAllSSL) {
+            try {
+                SSLContext SSL_CONTEXT = SSLContext.getInstance("SSL");
+                SSL_CONTEXT.init(null, new TrustManager[]{
+                        new X509TrustManager() {
+                            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                                return null;
+                            }
+
+                            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                            }
+
+                            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                            }
+
+                        }}, null);
+                HttpsURLConnection.setDefaultSSLSocketFactory(SSL_CONTEXT.getSocketFactory());
+
+                HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
+            } catch (NoSuchAlgorithmException | KeyManagementException e) {
+                throw new RuntimeException("Unable to initialise SSL context", e);
+            }
+        }
     }
 
     @Override
@@ -64,5 +101,13 @@ public class ShiroCasRealm extends CasRealm {
 
     public void setEncoding(String encoding) {
         this.encoding = encoding;
+    }
+
+    public boolean isTrustAllSSL() {
+        return trustAllSSL;
+    }
+
+    public void setTrustAllSSL(boolean trustAllSSL) {
+        this.trustAllSSL = trustAllSSL;
     }
 }
