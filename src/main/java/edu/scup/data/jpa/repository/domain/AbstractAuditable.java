0a014cf06fc69bb57edbb8804708f284df08cf64
phoenix <wujunchen@gmail.com>
2015-03-15 15:00:06 +0800

    由OMS定制的Auditable改为较通用的Auditable

---
+++ src/main/java/edu/scup/data/jpa/repository/domain/AbstractAuditable.java
@@ -0,0 +1,73 @@
+package edu.scup.data.jpa.repository.domain;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Abstract base class for auditable entities. Stores the audition values in persistent fields.
+ *
+ * @param <PK> the type of the auditing type's idenifier
+ * @see org.springframework.data.jpa.domain.AbstractAuditable
+ * audit 使用OMS的登陆用户名
+ */
+@MappedSuperclass
+public abstract class AbstractAuditable<U, PK extends Serializable> extends AbstractPersistable<PK>
+        implements Auditable<U, PK> {
+
+    private static final long serialVersionUID = -2363991186256484787L;
+
+    @ManyToOne
+    private U createdBy;
+
+    @ManyToOne
+    private U lastModifiedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    public U getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(final U createdBy) {
+
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(final Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @Override
+    public U getLastModifiedBy() {
+
+        return lastModifiedBy;
+    }
+
+    @Override
+    public void setLastModifiedBy(final U lastModifiedBy) {
+
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
