c09440a507e326be6a5b7d592694aaeec68e8cb1
phoenix <wujunchen@gmail.com>
2015-12-06 16:56:38 +0800

    bean属性拷贝时，支持排除字段

--- src/main/java/edu/scup/util/BeanUtils.java
+++ src/main/java/edu/scup/util/BeanUtils.java
@@ -4,6 +4,8 @@ import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.beanutils.PropertyUtilsBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeansException;
 
 import java.lang.reflect.InvocationTargetException;
 
@@ -26,5 +28,24 @@ public class BeanUtils extends org.apache.commons.beanutils.BeanUtils {
                 LOG.warn("can't find property {} of {}", propertyName, orig);
             }
         }
+    }
+
+    /**
+     * Copy the property values of the given source bean into the given target bean,
+     * ignoring the given "ignoreProperties".
+     * <p>Note: The source and target classes do not have to match or even be derived
+     * from each other, as long as the properties match. Any bean properties that the
+     * source bean exposes but the target bean does not will silently be ignored.
+     * <p>This is just a convenience method. For more complex transfer needs,
+     * consider using a full BeanWrapper.
+     *
+     * @param source           the source bean
+     * @param target           the target bean
+     * @param ignoreProperties array of property names to ignore
+     * @throws BeansException if the copying failed
+     * @see BeanWrapper
+     */
+    public static void copyPropertiesExclude(Object source, Object target, String... ignoreProperties) throws BeansException {
+        org.springframework.beans.BeanUtils.copyProperties(source, target, ignoreProperties);
     }
 }
